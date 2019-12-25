4959118231a2bad9560957469c98b0684ddb577f
dotantai1999 <dotantai.1999@gmail.com>
2019-12-25 14:58:30 +0700

    Update categories 2 levels - User Page

--- OneAuct/middlewares/locals.mdw.js
+++ OneAuct/middlewares/locals.mdw.js
@@ -2,9 +2,12 @@ const categoryModel = require('../models/category.model');
 
 module.exports = (app) => {
     app.use(async (req, res, next) => {
-        const rows = await categoryModel.allWithDetails();
-        res.locals.lcCategories = rows;
-
+        const parents = await categoryModel.all();
+        for(i = 0; i < parents.length; i++){
+            let categories = await categoryModel.allBySub(parents[i].CatID);
+            parents[i].SubCategories = categories;
+        }
+        res.locals.lcCategories = parents;
         if(typeof(req.session.isAuthenticated) === 'undefined') {
             req.session.isAuthenticated = false;
         }
